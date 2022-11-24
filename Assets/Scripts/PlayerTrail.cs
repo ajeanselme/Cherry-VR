a504188094b9aac45384ac15271b237367b36a62
illianes <pierremariotte98@mail.com>
2022-11-24 11:31:18 +0100

    Update trail

---
+++ Assets/Scripts/PlayerTrail.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerTrail : MonoBehaviour
+{
+
+   /* private float timerShadows;
+    public float startTimerShadows;
+
+    public GameObject shadow;
+    public InputActionProperty moveInput;
+
+
+
+    void Update()
+    {
+        if (moveInput.action.ReadValue<Vector2>().x != 0)
+        {
+            if (timerShadows >= 0.05f)
+            {
+                GameObject instance = (GameObject)Instantiate(shadow, transform.position, Quaternion.identity);
+                Destroy(instance, 0.1f);
+                timerShadows = startTimerShadows;
+            }
+            else
+                timerShadows += Time.deltaTime;
+        }
+    }*/
+}
